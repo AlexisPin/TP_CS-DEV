c5cfa32158903725ee458bdf27c0c7255d7eef64
Alexis <pinceminalexis@gmail.com>
2020-11-21 15:37:21 +0100

    exo bissextile

--- TP1_CS_DEV.py
+++ TP1_CS_DEV.py
@@ -1,1 +1,8 @@
-def P() : 
+year = int(input("Entrez l'année:"))
+strYear = str(year)
+if year % 4 == 0 and strYear[2] != 0 and strYear[3] != 0 or year % 400 == 0:
+    year = str(year)
+    print("L'année {} est une année bissextile!" .format(year))
+else:
+    print("L'année {} n'est pas une année bissextile!" .format(year))
+
