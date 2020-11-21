d98848ec4738c12d2c268be2b23615cfb4130226
Alexis <pinceminalexis@gmail.com>
2020-11-21 15:52:16 +0100

    exercice année bissextile

--- TP1_CS_DEV.py
+++ TP1_CS_DEV.py
@@ -1,6 +1,7 @@
 year = int(input("Entrez l'année:"))
 strYear = str(year)
-if year % 4 == 0 and strYear[2] != 0 and strYear[3] != 0 or year % 400 == 0:
+
+if year % 4 == 0 and strYear[2] != "0" or strYear[3] != "0" or year % 400 == 0:
     year = str(year)
     print("L'année {} est une année bissextile!" .format(year))
 else:
