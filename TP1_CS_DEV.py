e9d49ff81917c293936d77a6cb53e5f555e561bf
Alexis <pinceminalexis@gmail.com>
2020-11-23 17:40:57 +0100

    ff

--- TP1_CS_DEV.py
+++ TP1_CS_DEV.py
@@ -1,9 +1,12 @@
 year = int(input("Entrez l'année:"))
 strYear = str(year)
 
-if year % 4 == 0 and strYear[2] != "0" or strYear[3] != "0" or year % 400 == 0:
-    year = str(year)
-    print("L'année {} est une année bissextile!" .format(year))
-else:
-    print("L'année {} n'est pas une année bissextile!" .format(year))
 
+def bissextile(year):
+    if year % 4 == 0 and strYear[2] != "0" or strYear[3] != "0" or year % 400 == 0:
+        return True
+    else:
+        return False
+
+
+bissextile(year)
