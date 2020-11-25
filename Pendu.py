c5c1ee925fef45e5b4856ea758fd15d388398c6b
Alexis <pinceminalexis@gmail.com>
2020-11-25 21:59:31 +0100

    version sans commentaire

--- Pendu.py
+++ Pendu.py
@@ -11,14 +11,26 @@ word = random.choice(words)
 error = 0
 wordList = []
 myWordList = []
+bodyItems = ['O','/','|','\\','/','\\','']
+myBody = ['','','','','','','']
 for x in word:
     wordList.append(x)
 
 for x in word:
     myWordList.append("")
 
-while error < 8 :
+while error < 7 :
+    alreadyUse = False
+    print(' \n -----')
+    print(' |   |')
+    print(' |   {}' .format(myBody[0]))
+    print(' |  {}{}{}'.format(myBody[1],myBody[2],myBody[3]))
+    print(' |  {} {}'.format(myBody[4], myBody[5]))
+    print('_|_')
     letter = input('Choisissez une lettre : ')
+    if letter in letters:
+        alreadyUse = True
+        print('Letrre déjà proposé')
     letters.append(letter)
     if len(letter) > 1 and letter == word:
         print('Vous avez gagné !')
@@ -36,10 +48,11 @@ while error < 8 :
         print('Vous avez gagné !')
         break
 
-    if letter not in word:
+    if letter not in word and alreadyUse == False:
         error += 1
-        print('Il vous reste {} tentative(s)' .format(8 - error))
-        if error ==8:
+        myBody[error-1] = bodyItems[error-1]
+        print('Il vous reste {} tentative(s)' .format(7 - error))
+        if error ==7:
             print('Perdu ! Le mot qui fallait trouvé était {}' .format(word))
 
 
